8944c548f132c2f4bfa4d25efc6c416c92195e7e
Muhammad Charaf <mohamed.charaf@aurea.com>
2021-02-02 05:35:51 +0200

    Initializing environment variables

--- api/events-api/src/index.ts
+++ api/events-api/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
+import * as dotenv from 'dotenv';
 
-
+dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT || 8080;
 
