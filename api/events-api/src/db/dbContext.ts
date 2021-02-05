ffda71f6cd100c32c5be81cc8d6d08ccc864e4c5
Muhammad Charaf <muhammad.charaf@gmail.com>
2021-02-05 09:29:53 +0200

    Create Migrations

--- api/events-api/src/db/dbContext.ts
+++ api/events-api/src/db/dbContext.ts
@@ -1,11 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
+const dbconfig = require('../db/config/config.json');
 
 export async function makeDbContext() {
+  const config = dbconfig[process.env.NODE_ENV as string];
+
   const dbContext: Sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USERNAME as string,
-    process.env.DB_PASSWORD as string,
-    { host: process.env.DB_HOST, dialect: 'postgres' }
+    config.database,
+    config.username,
+    config.password,
+    { host: config.host, dialect: config.dialect }
   );
 
   try {
